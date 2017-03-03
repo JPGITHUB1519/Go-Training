6590394da3bf5003c846dd85907e1b21899d4fc5
Jean Urena <jpurena@intellisys.com.do>
2017-03-03 17:48:07 -0400

    Add Book Part

---
+++ Introducing-Go-Book/1- Introduction/HelloNyName.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre := "Jean"
+	fmt.Println("My name is", nombre)
+}
---
+++ Introducing-Go-Book/2- Types/Exercises/3.go
@@ -0,0 +1,14 @@
+/*
+
+   Although overpowered for the task, you can use Go as a calculator. Write a pro‐ gram that computes 32,132 × 42,452 and prints it to the terminal (use the * oper‐ ator for multiplication).
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	n1 := 32132
+	n2 := 42452
+	fmt.Println(n1 * n2)
+}
